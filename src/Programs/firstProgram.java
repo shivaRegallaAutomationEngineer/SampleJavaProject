f6808dd6b8bbf7d5b193bdbf49173c115956d21a
shivaRegallaAutomationEngineer <shivarrac@gmail.com>
2020-04-05 08:25:38 +0530

    made changes according the comments provided

---
+++ src/Programs/firstProgram.java
@@ -0,0 +1,16 @@
+package Programs; 
+public class firstProgram { 
+	public static void main(String args[]) {
+	
+	int a=100;
+	int b=50;
+	int c=300;
+	if((a<b)&(b<c)) {
+		System.out.println("C is bigger number");
+		
+	}else {
+		System.out.println("if the condition got failed please come here ");
+	}
+	 
+	}
+}
